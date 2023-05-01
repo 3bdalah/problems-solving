bf078ef2a7ea10c8164118dc675f7faf3bd73e9e
3bdalah <3bdallah.dev@gmail.com>
2023-05-01 03:03:20 +0300

    first problem

---
+++ problems-solved/first-problem/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    v.push_back(5);
+    v.push_back(3);
+    v.push_back(22);
+    v.push_back(12);
+    cout<<v[1]<<endl;
+    return 0;
+}
